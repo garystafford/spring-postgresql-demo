298d82dc15ec0be664de039fe6505bc341a9b620
Gary Stafford <gary.stafford@accenture.com>
2018-03-23 23:18:55 -0400

    Fix Vote Respository

--- src/main/java/pcfdemo/repository/VoteRepository.java
+++ src/main/java/pcfdemo/repository/VoteRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface VoteRepository extends CrudRepository<Vote, Long> {
 
-    List<Vote> findByElectionCandidate(String electionCandidate);
+//    List<Vote> findByElectionCandidate_Id(String Id);
 }
