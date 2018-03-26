a0f953b6f9a009a61bc38118dd996734a2a33277
Gary Stafford <gary.stafford@accenture.com>
2018-03-26 17:01:56 -0400

    Refactor Swagger config

--- src/main/java/pcfdemo/controller/CandidateController.java
+++ src/main/java/pcfdemo/controller/CandidateController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pcfdemo.model.Candidate;
 import pcfdemo.repository.CandidateRepository;
