d5227e93ee9a20fea0864cb7562f29261e5ce562
Gary Stafford <gary.stafford@accenture.com>
2018-03-30 20:48:35 -0400

    Adding function to generate a range of random votes for an election candidate

--- src/main/resources/db/changelog/changes/v0001.sql
+++ src/main/resources/db/changelog/changes/v0001.sql
@@ -52,6 +52,7 @@ CREATE TABLE IF NOT EXISTS vote
 ------------------------------
 -- generate_random_votes
 ------------------------------
+-- generate a random number of votes for all election candidates
 CREATE OR REPLACE FUNCTION generate_random_votes(n INTEGER DEFAULT 100)
   RETURNS VOID
 LANGUAGE plpgsql
@@ -66,6 +67,28 @@ BEGIN
          FROM election_candidate))
        LIMIT 1)
     );
+  END LOOP;
+END;
+$$;
+
+------------------------------
+-- generate_votes
+------------------------------
+-- generate a random number of votes within a range, for a specific election candidate
+CREATE OR REPLACE FUNCTION generate_votes(minVotes                   INTEGER DEFAULT 100,
+                                          maxVotes                   INTEGER DEFAULT 500,
+                                          electionToVoteIn           VARCHAR(100) DEFAULT 'NULL',
+                                          lastNameCandidateToVoteFor VARCHAR(50) DEFAULT 'NULL')
+  RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+BEGIN
+  FOR counter IN 1..(Cast(RANDOM() * (maxVotes - minVotes) + minVotes AS INT)) LOOP
+    INSERT INTO vote (election_candidate_id)
+    VALUES ((SELECT Id
+             FROM candidates_by_elections
+             WHERE (election LIKE electionToVoteIn) AND (last_name LIKE lastNameCandidateToVoteFor)
+    ));
   END LOOP;
 END;
 $$;
--- src/main/resources/db/changelog/changes/v0002.sql
+++ src/main/resources/db/changelog/changes/v0002.sql
@@ -137,7 +137,21 @@ INSERT INTO election_candidate (election_id, candidate_id) VALUES (
    WHERE last_name LIKE 'McConnell'
    LIMIT 1));
 
-------------------------------
--- generate 250 random votes
-------------------------------
-SELECT generate_random_votes(250);
+-----------------------------------
+-- generate completely random votes
+-----------------------------------
+SELECT generate_random_votes(1000);
+
+----------------------------------------------------------
+-- generate random votes for specific electoral candidates
+----------------------------------------------------------
+SELECT generate_votes(1700, 2000, '2012 Presidential Election', 'Obama');
+SELECT generate_votes(600, 1400, '2012 Presidential Election', 'Romney');
+SELECT generate_votes(300, 500, '2012 Presidential Election', 'Stein');
+
+SELECT generate_votes(2000, 3000, '2016 Presidential Election', 'Clinton');
+SELECT generate_votes(1500, 1900, '2016 Presidential Election', 'Trump');
+
+SELECT generate_votes(1000, 1200, '2017 New York City Mayoral Election', 'de Blasio');
+SELECT generate_votes(700, 900, '2018 New York Gubernatorial Election', 'Cuomo');
+SELECT generate_votes(200, 500, '2018 United States Senate Elections', 'McConnell');
