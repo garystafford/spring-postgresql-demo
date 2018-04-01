9ea8719ab48562e1af80d7f8ae55d8d7a03adba7
Gary A. Stafford <garystafford@rochester.rr.com>
2018-04-01 09:35:06 -0400

    Refactor Docker script

--- docker-build.sh
+++ docker-build.sh
@@ -3,4 +3,5 @@
 # docker build -t spring-postgresql-demo:$(git rev-parse --short HEAD) .
 docker build --no-cache -t spring-postgresql-demo:latest .
 
+docker rm -f postgres springdemo
 docker-compose -p springdemo up -d
