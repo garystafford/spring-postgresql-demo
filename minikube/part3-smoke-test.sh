ca3d5780f0655b9cbeca9f41e228de12853e5ffc
Gary A. Stafford <garystafford@rochester.rr.com>
2018-04-15 11:52:48 -0400

    Refactor smoke test script

--- minikube/part3-smoke-test.sh
+++ minikube/part3-smoke-test.sh
@@ -4,7 +4,8 @@
 # https://istio.io/docs/guides/bookinfo.html
 # adjust for minikube ip
 
-export GATEWAY_URL=$(minikube ip):$(kubectl get svc istio-ingress -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
+export GATEWAY_URL="$(minikube ip):"\
+"$(kubectl get svc istio-ingress -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')"
 echo $GATEWAY_URL
 
 curl $GATEWAY_URL/v2/actuator/health && echo
