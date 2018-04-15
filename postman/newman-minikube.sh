303b636dd12f51dd3ce31765d829a3107c993e38
Gary A. Stafford <garystafford@rochester.rr.com>
2018-04-14 20:11:56 -0400

    Adding minikube env for Postman

--- postman/newman-minikube.sh
+++ postman/newman-minikube.sh
@@ -6,4 +6,5 @@ export ENVIRONMENT="spring-postgresql-demo_gke_minikube.postman_environment.json
 newman run $COLLECTION \
   --environment $ENVIRONMENT \
   --iteration-count 5 \
-  --delay-request 250
+  --delay-request 250 \
+  --no-color
