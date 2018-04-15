501c4a6e6454507325932937abde281bde4348f8
Gary A. Stafford <garystafford@rochester.rr.com>
2018-04-14 21:25:58 -0400

    Adding JUnit reports to Newman

--- postman/newman-minikube.sh
+++ postman/newman-minikube.sh
@@ -6,4 +6,5 @@ export ENVIRONMENT="spring-postgresql-demo_gke_minikube.postman_environment.json
 newman run $COLLECTION \
   --environment $ENVIRONMENT \
   --iteration-count 1 \
-  --no-color
+  --no-color \
+  --reporters cli,junit
