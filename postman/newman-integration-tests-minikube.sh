364387e8e14a917b34ad6ed08b8a45ddab2f5dc2
Gary A. Stafford <garystafford@rochester.rr.com>
2018-04-17 22:42:26 -0400

    Refactor tests into two collections

--- postman/newman-integration-tests-minikube.sh
+++ postman/newman-integration-tests-minikube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COLLECTION="spring-postgresql-demo-v2.postman_collection.json"
+COLLECTION="election-k8s-integration.postman_collection.json"
 ENVIRONMENT="spring-postgresql-demo_gke_minikube.postman_environment.json"
 ITERATIONS=1
 DELAY=250
---
+++ postman/newman-smoke-tests-minikube.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+COLLECTION="election-k8s-smoke.postman_collection.json"
+ENVIRONMENT="spring-postgresql-demo_gke_minikube.postman_environment.json"
+ITERATIONS=1
+DELAY=250
+
+newman run $COLLECTION \
+  --environment $ENVIRONMENT \
+  --iteration-count $ITERATIONS \
+  --no-color \
+  --reporters cli,junit
