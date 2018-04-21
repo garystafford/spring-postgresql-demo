b50667a493bb909a71bc2896b7dc7058ecffdbb3
Gary A. Stafford <garystafford@rochester.rr.com>
2018-04-20 23:26:45 -0400

    Starting to add JUnit tests

---
+++ src/test/java/pcfdemo/SwaggerConfigTest.java
@@ -0,0 +1,26 @@
+package pcfdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerConfigTest {
+
+    @Autowired
+    private SwaggerConfig swaggerConfig;
+
+    @Test
+    public void apiInfo_Docket_DocumentationType_Swagger() {
+        Docket docket = swaggerConfig.api();
+        assertThat(docket.getDocumentationType().getName().equals("swagger")).isTrue();
+        assertThat(docket.getDocumentationType().getVersion().equals("2.0")).isTrue();
+    }
+
+}
