5a41cdb9e671dd86a85fb2813306d0a17f058211
Gary A. Stafford <garystafford@rochester.rr.com>
2018-04-26 13:39:12 -0400

    Fixing Postgres connection

--- kubernetes/part3a-deploy-v1-dev.sh
+++ kubernetes/part3a-deploy-v1-dev.sh
@@ -5,8 +5,7 @@
 # election v1 deployment with manual sidecar injection
 # kubectl apply -f ./deployments/election-deployment-v1-dev.yaml
 istioctl kube-inject –kubeconfig "~/.kube/config" \
-  -f ./deployments/election-deployment-v1-dev.yaml \
-  --includeIPRanges=$IP_RANGES > \
+  -f ./deployments/election-deployment-v1-dev.yaml > \
   election-deployment-istio.yaml \
   && kubectl apply -f election-deployment-istio.yaml \
   && rm election-deployment-istio.yaml
