4866380128fbbf99902124986428f2b1318406f0
Gary A. Stafford <garystafford@rochester.rr.com>
2018-04-27 08:25:01 -0400

    Script and Jenkinsfile rectoring, project cleanup

--- kubernetes/README.md
+++ kubernetes/README.md
@@ -1,4 +1,4 @@
-# Kubernetes Installation
+# Kubernetes Installation Notes
 
 ## Amazon RDS
 
--- kubernetes/part3a-deploy-v1-dev.sh
+++ kubernetes/part3a-deploy-v1-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# apply resources part 3a: v1 to dev
+# apply resources part 3a: just v1 to dev
 
 # election v1 deployment with manual sidecar injection
 # kubectl apply -f ./deployments/election-deployment-v1-dev.yaml
