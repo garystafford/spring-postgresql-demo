67979acda153ba8735390f091cb9c51f75b47f80
Gary A. Stafford <garystafford@rochester.rr.com>
2018-05-13 09:34:04 -0400

    Fixing typo in test class

--- src/test/java/com/voter_demo/controller/CandidateControllerTest.java
+++ src/test/java/com/voter_demo/controller/CandidateControllerTest.java
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class git CandidateControllerTest {
+public class CandidateControllerTest {
 
     @Autowired
     private CandidateController candidateController;
