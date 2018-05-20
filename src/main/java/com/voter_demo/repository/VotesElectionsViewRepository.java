af6f295edc1d662e0206f12ac608c16352d756d8
Gary A. Stafford <garystafford@rochester.rr.com>
2018-05-20 19:16:21 -0400

    Adding additional respository tests

--- src/main/java/com/voter_demo/repository/VotesElectionsViewRepository.java
+++ src/main/java/com/voter_demo/repository/VotesElectionsViewRepository.java
@@ -1,6 +1,5 @@
 package com.voter_demo.repository;
 
-import com.voter_demo.model.ElectionsCandidatesView;
 import com.voter_demo.model.VotesElectionsView;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
